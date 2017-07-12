586d9c95528d9805da0f3b3a3e84c2f2429a4a59
SkalskiP <pskalski@student.agh.edu.pl>
2017-07-12 21:33:49 +0200

    Polynomial Regression R

--- Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression_MINE.R
+++ Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression_MINE.R
@@ -23,6 +23,7 @@ lin_reg = lm(formula = Salary ~ .,
 # Fitting Polynomial Regression to the dataset
 dataset$Level2 = dataset$Level^2
 dataset$Level3 = dataset$Level^3
+dataset$Level4 = dataset$Level^4
 poly_reg = lm(formula = Salary ~ .,
               data = dataset)
 
@@ -39,3 +40,20 @@ ggplot() +
 
 
 # Visualising the Polynomial Regression results
+ggplot() +
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') +
+  geom_line(aes(x = dataset$Level, y = predict(poly_reg, newdata = dataset)),
+            colour = 'blue') +
+  ggtitle('Truth or Bluff (Polynomial Regression)') +
+  xlab('Level') +
+  ylab('Salary')
+
+# Predicting a new result with Linear Regression
+y_pred = predict(lin_reg, data.frame(Level = 6.5))
+
+# Predicting a new result with Polynomial Regression
+y_pred = predict(poly_reg, data.frame(Level = 6.5,
+                                      Level2 = 6.5^2,
+                                      Level3 = 6.5^3,
+                                      Level4 = 6.5^4))
