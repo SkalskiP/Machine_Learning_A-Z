a99e83127cdb2a9eb610bdb0966014f7d01401d6
SkalskiP <pskalski@student.agh.edu.pl>
2017-08-28 22:43:09 +0200

    Hierarchical Clustering Python and R done Apriori data download

--- Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans_MINE.R
+++ Part 4 - Clustering/Section 24 - K-Means Clustering/kmeans_MINE.R
@@ -1,4 +1,29 @@
 # K-Means Clustering
 
 # Importing the mall dataset
-daaset <- read.csv("mall.csv")
+dataset <- read.csv("Mall_Customers.csv")
+X <- dataset[4:5]
+
+# Using the elbow method to find the optimal number of clusters
+set.seed(6)
+wcss <- vector()
+for(i in 1:10) wcss[i] <- sum(kmeans(X, i)$withinss)
+plot(1:10, wcss, type = "b", main = paste('Clusters of clients'), xlab = "Number of clusters", ylab = "WCSS")
+
+# Applying k-means to the mall dataset
+set.seed(29)
+kmeans <- kmeans(X, 5, iter.max = 300, nstart = 10)
+
+# Visualising the clasters
+library(cluster)
+clusplot(X,
+         kmeans$cluster,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Clusters of clients'),
+         xlab = "Annual Income",
+         ylab = "Spending Score")
---
+++ Part 4 - Clustering/Section 25 - Hierarchical Clustering/hc_MINE.R
@@ -0,0 +1,4 @@
+# Hierarchical Clustering
+
+# Impoting the mall dataset
+dataset = read.csv('Mall_Customers.csv')
