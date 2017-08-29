e2dd611861778442f7438f7d584dc0dc06d97e9c
SkalskiP <pskalski@student.agh.edu.pl>
2017-08-29 20:52:19 +0200

    Apriori models Python and R done

--- Part 4 - Clustering/Section 25 - Hierarchical Clustering/hc_MINE.R
+++ Part 4 - Clustering/Section 25 - Hierarchical Clustering/hc_MINE.R
@@ -2,3 +2,29 @@
 
 # Impoting the mall dataset
 dataset = read.csv('Mall_Customers.csv')
+X = dataset[4:5]
+
+# Using the dendrogram to find the optimal number of clusters
+dendrogram = hclust(dist(X, method = 'euclidean'), method = 'ward.D')
+plot(dendrogram,
+     main = paste('Dendrogram'),
+     xlab = 'Customers',
+     ylab = 'Euclidean distance')
+
+# Fitting hierarchical clustering to the mall dataset
+hc = hclust(dist(X, method = 'euclidean'), method = 'ward.D')
+y_hc = cutree(hc, 5)
+
+# Visualising the clusters
+library(cluster)
+clusplot(dataset,
+         y_hc,
+         lines = 0,
+         shade = TRUE,
+         color = TRUE,
+         labels = 2,
+         plotchar = FALSE,
+         span = TRUE,
+         main = paste('Clusters of customers'),
+         xlab = 'Annual Income',
+         ylab = 'Spending Score')
